b0cc324d12d8049c3b66f72da971d2b19e7ea954
vaxelben <benjamin@datawrap.fr>
2025-03-04 20:30:16 +0100

    create posts graph

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Post } from "./pages/Post";
+import { Posts } from "./pages/Posts";
 import { CustomGraphView } from "./pages/CustomGraphView";
 import { LombardiGraph3D } from "./pages/LombardiGraph3D";
 import { DebugProvider } from "./hooks/useDebugMode";
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<CustomGraphView />} />
           <Route path="/post" element={<Post />} />
           <Route path="/lombardi-graph-3d" element={<LombardiGraph3D />} />
+          <Route path="/posts" element={<Posts />} />
         </Routes>
       </Router>
     </DebugProvider>
