0b555d5c762c140dc3af4423f9bb0a424d3ffd96
vaxelben <benjamin@datawrap.fr>
2025-03-07 15:51:18 +0100

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['d3-force']
-  },
   build: {
-    commonjsOptions: {
-      include: [/d3-force/, /node_modules/]
+    rollupOptions: {
+      external: ['d3-force']
     }
   }
 })
